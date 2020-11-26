7b19fc8c3c779559aaaad98aa9187ce4fdee8b55
Abdulaziz Sirojiddinov <aziz@SCPC-001.localdomain>
2020-11-26 15:29:36 +0900

    erc20 token src commit

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+
+
+'use strict';
+
+const tokenERC20Contract = require('./src/tokenERC20.js');
+
+module.exports.TokenERC20Contract = tokenERC20Contract;
+module.exports.contracts = [tokenERC20Contract];
